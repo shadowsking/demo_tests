924005e630daf04941f7eb185c3c517198a9b5c2
shadowsking <shadowsking1295@gmail.com>
2025-03-17 05:45:50 +0300

    Added unittest.mock

---
+++ demo_unittest_mock/test_random.py
@@ -0,0 +1,17 @@
+import random
+from unittest.mock import patch
+
+
+def roll():
+    return random.randint(1, 100)
+
+
+def test_roll_one():
+    with patch("demo_unittest_mock.test_random.random.randint", return_value=5):
+        assert roll() == 5
+
+
+@patch("demo_unittest_mock.test_random.random.randint")
+def test_roll_two(mock):
+    mock.return_value = 100
+    assert roll() == 100
